fa2aaec5619adc2f6901d0586c0df5ef266000bb
Josh Swan <joshuawswan@gmail.com>
2017-10-02 22:25:35 -0700

    Initial commit

---
+++ netflix-unpause.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Netflix Unpause
+// @namespace   *://www.netflix.com
+// @version     1.0.0
+// @grant       none
+// @description Automatically clicks "continue playing"
+// ==/UserScript==
+setInterval(function(){}, 9999); //Dummy SetInterval (Gets cleared during netflix's load events)
+setInterval(function(){try{document.getElementsByClassName("interrupter-actions")[0].getElementsByClassName("nf-flat-button")[0].click()}catch(e){}}, 33);
